37fd47a6746e816bc533a2efa1f196f0c808151b
Cruz <cruz_ch96@outlook.com>
2022-11-23 23:56:14 +0800

    commit storyblok changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+    modules: [
+      [
+        '@storyblok/nuxt',
+        {
+                  accessToken: process.env.SB_API_TOKEN,
+                  // If Server Location === US then:
+                  // apiOptions: { region: 'us' }
+              },
+             
+      ],
+      '@nuxtjs/tailwindcss'
+    ],
+    experimental: {
+      payloadExtraction: false
+    }
+  
+  })
